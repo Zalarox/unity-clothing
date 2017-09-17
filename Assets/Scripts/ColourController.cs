19a02bc3c082c1ae3533b728d4b22a48c21abb3b
Siddhant <bansal.siddhant@gmail.com>
2017-09-17 11:48:14 +0530

    Better handling of colours

--- Assets/Scripts/ColourController.cs
+++ Assets/Scripts/ColourController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class ColourController : MonoBehaviour {
@@ -9,7 +10,21 @@ public class ColourController : MonoBehaviour {
     Renderer shirtR, pantsR;
     Equipment equipment;
 
-    private int i = 0;
+    Dictionary<string, Color32> clothColours;
+    string shirtColour;
+
+    private enum Combination { None, Good, Bad };
+    private bool choosePants = false;
+    private Combination currentCombination = Combination.None;
+    
+    void InitialiseColours()
+    {
+        clothColours = new Dictionary<string, Color32>();
+        clothColours.Add("blue", new Color32(150, 208, 255, 255));
+        clothColours.Add("green", new Color32(204, 255, 150, 255));
+        clothColours.Add("pink", new Color32(255, 175, 255, 255));
+        clothColours.Add("orange", new Color32(255, 218, 61, 255));
+    }
 
     void Start()
     {
@@ -20,23 +35,48 @@ public class ColourController : MonoBehaviour {
         EquipItem("Chest", "shirt2");
         shirt = gameObject.transform.Find("shirt2").gameObject;
         pants = gameObject.transform.Find("pants2").gameObject;
-        //shirtR = shirt.GetComponent<Renderer>();
-        //pantsR = pants.GetComponent<Renderer>();
+        InitialiseColours();
+    }
+
+    Combination CheckCombination(string shirt, string pant)
+    {
+        if(pant == "blue" || pant == "orange")
+        {
+            return Combination.Good;
+        }
+        else
+        {
+            return Combination.Bad;
+        }
     }
 
     void ChangeQuestion()
     {
-        questionText.text = "What should be the colour of the trousers?";
+        if (choosePants == false)
+        {
+            questionText.text = "What should be the colour of the shirt?";
+        }
+        else if (choosePants == true && currentCombination == Combination.None)
+        {
+            questionText.text = "What should be the colour of the trousers?";
+        }
+        else if (choosePants == true && currentCombination == Combination.Good)
+        {
+            questionText.text = "That is a good combination! Well done.";
+        }
+        else
+        {
+            questionText.text = "That is not a good combination, try again!";
+        }
     }
 
     IEnumerator Upload()
     {
         WWWForm form = new WWWForm();
         form.AddField("name", "Siddhant");
-        //form.AddField("score", score);
-        form.AddField("gametype", "2D");
+        form.AddField("colorcombination", "");
 
-        WWW req = new WWW("localhost:3000/api/newshapedata", form);
+        WWW req = new WWW("localhost:3000/api/colourcombination", form);
         yield return req;
 
         if (!string.IsNullOrEmpty(req.error))
@@ -53,58 +93,80 @@ public class ColourController : MonoBehaviour {
 
     public void ChangeColour(string newColor)
     {
-        if(newColor.ToLower().Equals("blue"))
+        newColor = newColor.ToLower();
+
+        if(!clothColours.ContainsKey(newColor))
         {
-            if(i==0)
-            {              
-                shirt.renderer.material.SetColor("_Color", new Color32(150, 208, 255, 255));
-                i = 1;
+            Debug.LogError("Specified colour not found.");
+        }
+        else
+        {
+            if(choosePants == false)
+            {
+                shirt.renderer.material.SetColor("_Color", clothColours[newColor]);
+                choosePants = true;
+                shirtColour = newColor;
                 ChangeQuestion();
             }
             else
             {
-                pants.renderer.material.SetColor("_Color", new Color32(150, 208, 255, 255));
-            }
-        }
-        else if (newColor.ToLower().Equals("orange"))
-        {
-            if (i == 0)
-            {
-                shirt.renderer.material.SetColor("_Color", new Color32(255, 218, 61, 255));
-                i = 1;
+                pants.renderer.material.SetColor("_Color", clothColours[newColor]);
+                currentCombination = CheckCombination(shirtColour, newColor);
                 ChangeQuestion();
             }
-            else
-            {
-                pants.renderer.material.SetColor("_Color", new Color32(255, 218, 61, 255));
-            }
         }
-        else if (newColor.ToLower().Equals("pink"))
-        {
-            if (i == 0)
-            {
-                shirt.renderer.material.SetColor("_Color", new Color32(255, 175, 255, 255));
-                i = 1;
-                ChangeQuestion();
-            }
-            else
-            {
-                pants.renderer.material.SetColor("_Color", new Color32(255, 175, 255, 255));
-            }
-        }
-        else if (newColor.ToLower().Equals("green"))
-        {
-            if (i == 0)
-            {
-                shirt.renderer.material.SetColor("_Color", new Color32(204, 255, 150, 255));
-                i = 1;
-                ChangeQuestion();
-            }
-            else
-            {
-                pants.renderer.material.SetColor("_Color", new Color32(204, 255, 150, 255));
-            }
-        }
+        //if(newColor.ToLower().Equals("blue"))
+        //{
+        //    if(choosePants == false)
+        //    {              
+        //        shirt.renderer.material.SetColor("_Color", new Color32(150, 208, 255, 255));
+        //        choosePants = true;
+        //        ChangeQuestion();
+        //    }
+        //    else
+        //    {
+        //        pants.renderer.material.SetColor("_Color", new Color32(150, 208, 255, 255));
+        //    }
+        //}
+        //else if (newColor.ToLower().Equals("orange"))
+        //{
+        //    if (choosePants == false)
+        //    {
+        //        shirt.renderer.material.SetColor("_Color", new Color32(255, 218, 61, 255));
+        //        choosePants = true;
+        //        ChangeQuestion();
+        //    }
+        //    else
+        //    {
+        //        pants.renderer.material.SetColor("_Color", new Color32(255, 218, 61, 255));
+        //    }
+        //}
+        //else if (newColor.ToLower().Equals("pink"))
+        //{
+        //    if (choosePants == false)
+        //    {
+        //        shirt.renderer.material.SetColor("_Color", new Color32(255, 175, 255, 255));
+        //        choosePants = true;
+        //        ChangeQuestion();
+        //    }
+        //    else
+        //    {
+        //        pants.renderer.material.SetColor("_Color", new Color32(255, 175, 255, 255));
+        //    }
+        //}
+        //else if (newColor.ToLower().Equals("green"))
+        //{
+        //    if (choosePants == false)
+        //    {
+        //        shirt.renderer.material.SetColor("_Color", new Color32(204, 255, 150, 255));
+        //        choosePants = true;
+        //        ChangeQuestion();
+        //    }
+        //    else
+        //    {
+        //        pants.renderer.material.SetColor("_Color", new Color32(204, 255, 150, 255));
+        //    }
+        //}
     }
 
 
